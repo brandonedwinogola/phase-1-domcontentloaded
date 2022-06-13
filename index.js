46451e7918654449a8c7243af5126c55b39e6f53
bran don <106029484+brandonedwinogola@users.noreply.github.com>
2022-06-13 12:05:42 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("After DOM Loaded");
+    console.log("document.querySelector" ('Div'))
+
+});
+
+console.log("Before DOM Loads")
+console.log("document.querySelector" ('Div'))
+
+fuction(Javascript is so cool("This is really cool!")) {
+    let p = document.querySelector('p')
+
+    p.textContent = This is really cool!
+        document.querySelector('# This is really cool!_container').appendChild(p)
+}
